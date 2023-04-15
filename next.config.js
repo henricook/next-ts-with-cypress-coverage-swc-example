dc53bf70c755014c6f0e993e9e4fb986b5bfef7f
Henri Cook <henri.cook@linklaters.com>
2023-04-15 18:28:00 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const coveragePluginOptions = {
+  instrumentLog: {
+    level: 'trace',
+    enableTrace: true
+  }
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    swcPlugins: [
+      ["swc-plugin-coverage-instrument", coveragePluginOptions]
+    ]
+  }
 }
 
 module.exports = nextConfig
